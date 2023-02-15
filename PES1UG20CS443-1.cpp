623e1e285960bae8691c69ec93d500366112142b
Sukruth K <80237190+sukruthkgowda@users.noreply.github.com>
2023-02-15 13:55:28 +0530

    creating

---
+++ PES1UG20CS443-1.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout <<"This is Sukruth Keshava Gowda(PES1UG20CS443)":
+  cout <<"I'm in Section H \n";
+  cout << "Creating a  Pipeline \n";
+  return 0;
+}
