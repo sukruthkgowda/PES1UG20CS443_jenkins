38e06e16d282b73735da37accfe670aca5db2607
Sukruth K <80237190+sukruthkgowda@users.noreply.github.com>
2023-02-15 13:34:12 +0530

    third commit

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,6 +6,7 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "This is Sukruth Keshava Gowda(PES1UG20CS443)";
+  cout << "I am in H section";
   cout << "I have successfully built and run\n";
   return 0;
 }
