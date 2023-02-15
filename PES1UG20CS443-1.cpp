d5daa2c47a165f850530b92a8cd9fcf6567f16d4
Sukruth K <80237190+sukruthkgowda@users.noreply.github.com>
2023-02-15 14:01:01 +0530

    commit two

--- PES1UG20CS443-1.cpp
+++ PES1UG20CS443-1.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 int main(void) {
-  cout <<"This is Sukruth Keshava Gowda(PES1UG20CS443)":
+  cout <<"This is Sukruth Keshava Gowda(PES1UG20CS443)";
   cout <<"I'm in Section H \n";
   cout << "Creating a  Pipeline \n";
   return 0;
